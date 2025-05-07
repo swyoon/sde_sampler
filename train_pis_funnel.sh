b50c7c6a8000d0532c06389d5f16df3000e71779
hyeokjujeong <hyeokju0502@gmail.com>
2025-05-07 19:19:11 +0900

    Add funnel experiment script

---
+++ train_pis_funnel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export CUDA_VSIBLE_DEVICES=0
+
+for seed in 1 2 3
+do
+    echo "Running with seed=$seed"
+    python scripts/main.py seed=$seed target=funnel solver=pis loss.method=lv
+done
